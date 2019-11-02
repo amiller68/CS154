4538b9cfc67d1be7f18541584c55519fff4a97ab
amiller68 <amiller68@5d5a3bcd-ea47-4cdf-926e-b7c62f16f878>
2019-11-02 18:25:32 +0000

    Memory Infrastructure

--- p3cache/csim.c
+++ p3cache/csim.c
@@ -1,4 +1,5 @@
 #define _GNU_SOURCE
+#include <math.h>
 #include "cachelab.h"
 #include <unistd.h>
 #include <stdlib.h>
@@ -6,20 +7,44 @@
 #include <getopt.h>
 #include <string.h>
 
-typedef struct inst{
+typedef struct instr{
 	char inst;
 	int addr;
-	int val;
 } instr;
 
-typedef struct cac{
+typedef struct Block{
+	int val;
+	int tag;
+	int b;
+} Block;
+
+typedef struct lru_ind{
+	int ind;
+	struct lru_ind *next;
+} lru_ind;
+
+typedef struct LRU{
+	lru_ind *head;
+	lru_ind *tail;
+} LRU;
+
+typedef struct Set{
+	int E;
+	Block *B;
+	LRU *L;	
+} Set;
+
+typedef struct Cache{
 	int S;
 	int E;
 	int B;
-} cache;
+	Set *Se;
+} Cache;
 
+void printCache(Cache *C);
+Cache *createCache(int s, int e, int b);
 void lineToinstr(char *line, instr *i);
-void exec(cache *C, instr *i);
+void exec(Cache *C, instr *i);
 
 int main(int argc, char **argv)
 {
@@ -33,7 +58,7 @@ int main(int argc, char **argv)
 	size_t len = 0;
 	ssize_t read = 0;	
 	instr *in = malloc(sizeof(instr));
-
+	Cache *C = NULL;
 	while((res = getopt(argc, argv, "vs:e:b:t:")) != -1){
 		switch(res){
 			case 'v':
@@ -56,7 +81,10 @@ int main(int argc, char **argv)
 				exit(1);
 		}
 	}
-
+	
+	C = createCache(s, e, b);
+	printCache(C);
+	
 	trace = fopen(t,"r");
 	if (trace== NULL){ printf("No Trace\n"); exit(1);}
   	
@@ -65,22 +93,82 @@ int main(int argc, char **argv)
 		lineToinstr(line, in);
     }
 	
+	
 	free(in);
 	free(line);
 	fclose(trace);
 
-	printf("-v=%d\n-s=%d\n-e=%d\n-b=%d\n-t=%s\n", verb, s, e, b, t);
 
-	
-	
-	
 
-//allocate size of cache
-//-> grid? or array? multi array for associativity
-//hit count // miss count
-//create -v option
     printSummary(mc, hc, ec);
     return 0;
+}
+
+Cache *createCache(int s, int e, int b){
+	int S = pow(2, s);
+	int E = e;
+	int B = pow(2, b);
+	
+	Cache *C = malloc(sizeof(Cache));
+	Block *bl = malloc(S*E*sizeof(Block));
+	lru_ind *l = malloc(S*E*sizeof(lru_ind));
+	Set *Se = malloc(S*sizeof(Set));
+	LRU *L = malloc(S*sizeof(LRU));
+	
+	int ind = -1;
+	int i = -1;
+	//fill init values of lru_indices + blocks
+	for(i = 0; i < S*E; i++){
+		bl[i].val = 0;
+		bl[i].tag = 0;
+		bl[i].b = b;
+		ind = i % E;
+		l[i].ind = ind;
+		if(ind == (E -1)) l[i].next = NULL;
+		else l[i].next = &l[i] + 1;		
+	} 	
+
+	for(i=0; i<S; i++){
+		Se[i].E = E;
+		Se[i].B = bl;
+		//inc b pointer by E indices
+		bl += E;
+		L->head = l;
+		L->tail = l + (E - 1);
+		Se[i].L = L;
+		l += E;		
+		L++; 
+	}
+
+	C->S = S;
+	C->E = E;
+	C->B = B;
+	C->Se = Se;
+	
+	return C;
+}
+
+void printCache(Cache *C){
+	printf("S: %d | E: %d | B: %d\n", C->S, C->E, C->B);
+	int i = -1;
+	int j = -1;
+	Set *Se = NULL;
+	Block *bl = NULL;
+	for(i=0; i < C->S; i++){
+		Se = C->Se;
+		printf("SET %d:\n", i);
+		lru_ind *l = (Se->L)->head;
+		while(l != NULL){
+			printf("%d->", l->ind);			
+			l = l->next;
+ 		}
+		printf("\n");
+		
+		for(j=0; j < Se->E; j ++){
+			bl = Se->B;
+			printf("b %d: %d %d %d\n", j, bl[j].val, bl[j].tag, bl[j].b);
+		}
+	}
 }
 
 void lineToinstr(char *line, instr *i){
@@ -89,10 +177,7 @@ void lineToinstr(char *line, instr *i){
 	ch = strsep(&line, " ");
 	i->inst = ch[0];
 	ch = strsep(&line, ",");
-	i->addr = atoi(ch);
-	ch = strsep(&line, "");
-	i->val = atoi(ch);	
-
+	i->addr = strtol(ch, NULL, 16);
 	return;
 }
 
