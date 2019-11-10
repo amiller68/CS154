062269e6e0a4e530300e3fd07603822824d1537b
amiller68 <amiller68@5d5a3bcd-ea47-4cdf-926e-b7c62f16f878>
2019-11-10 23:01:21 +0000

    Its closer

--- p3cache/trans.c
+++ p3cache/trans.c
@@ -22,6 +22,14 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int i, j, tmp;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            tmp = A[i][j];
+            B[j][i] = tmp;
+        }
+    }
+
 }
 
 /*
@@ -33,18 +41,34 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 
-char trans_desc[] = "Simple row-wise scan transpose";
+char trans_desc[] = "Square by square trasnpose";
 void trans(int M, int N, int A[N][M], int B[M][N])
 {
-    int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
+//what do you want to do?
+// find how many ints you can load into a block 
+// b = 5, s  = 5
+// 4 byte ints
+// therefore 8 ints per block
+// operate on matrices by 8*8 blocks
+// a blocks inc column wise
+// b blcoks row wise 
+
+    int g, h, i, j, k, l;
+
+	for(g = 0; g < N; g+=32){
+	for(h = 0; h < M; h +=32){
+    for (j = h; j < 32+h; j+=8) {
+        for (i = g; i < 32+g; i+=8) {
+            for (k = 0; k < 8; k++){
+				for (l = 0; l < 8; l++){
+					B[j+k][i+l] = A[i+l][j+k];		
+				}
+			}
         }
     }
-
+	}
+	}
 }
 
 /*
